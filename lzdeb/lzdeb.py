18d057ebb49cd9be148fc19d1ad7eda06c3c6554
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-11 09:30:58 +0000

    Adding tarfile member sanitization to extractall()

--- lzdeb/lzdeb.py
+++ lzdeb/lzdeb.py
@@ -190,7 +190,26 @@ class DockerContainer:
                 for chunk in data_chunks:
                     f.write(chunk)
             with tarfile.open(collected_archive, mode='r') as tf:
-                tf.extractall(path=dest_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tf, path=dest_dir)
             return os.path.join(dest_dir, os.path.basename(src_path))
         finally:
             if os.path.exists(collected_archive):
